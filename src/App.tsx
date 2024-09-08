99889087c97bd7c32ab7bd2c922362ce8ec70069
a949066041 <949066041@qq.com>
2024-09-08 20:49:46 +0800

    chore: update dep

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,12 @@ import './App.css'
 
 function App() {
   return (
-    <div className="i-hugeicons:acceleration"></div>
+    <div className="h-100vh w-full flex items-center justify-center">
+      <div className="flex items-center">
+        <span className="mr-2 text-3xl text-red-5">hello template</span>
+        <div className="i-material-symbols:10k-outline h-1em w-1em"></div>
+      </div>
+    </div>
   )
 }
 
