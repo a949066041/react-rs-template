50505b2ea42e6d53f1db490f6abb6e65e563141f
a949066041 <wangsh0508@gmail.com>
2024-10-22 17:20:34 +0800

    chore: update dep add store

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { useCountStore } from './store'
 import './App.css'
 
 function App() {
+  const { count, increment, decrement } = useCountStore()
+
   return (
     <div className="h-100vh w-full flex items-center justify-center">
       <div className="flex items-center">
-        <span className="mr-2 text-3xl text-red-5">hello template</span>
+        <span className="mr-2 text-3xl text-red-5">
+          hello template
+          { count }
+        </span>
         <div className="i-material-symbols:10k-outline h-1em w-1em"></div>
+        <button onClick={() => increment(1)} type="button">click me</button>
+        <button onClick={() => decrement(1)} type="button">click me2</button>
       </div>
     </div>
   )
